bc6f262f2bcab50e3e6e08a15376efeb1cd3f90d
scuba75 <scuba75@gmail.com>
2024-06-25 10:25:29 -0500

    update

--- src/sync/checkComplete.js
+++ src/sync/checkComplete.js
@@ -4,7 +4,7 @@ const mongo = require('mongoclient')
 const sorter = require('json-array-sorter')
 module.exports = async(gaEvent = {})=>{
   if(!gaEvent?.season) return
-  let obj = await mongo.find('gaEvents', {}, { leaderboardScanComplete: 1, season: 1, key: 1, _id: 1, endTime: 1, date: 1, counterScanComplete: 1 })
+  let obj = await mongo.find('gaEvents', {}, { historyScanComplete: 1, leaderboardScanComplete: 1, season: 1, key: 1, _id: 1, endTime: 1, date: 1, counterScanComplete: 1 })
   if(!obj || obj.length == 0) return
   obj = sorter([{ column: 'endTime', order: 'descending' }], obj.filter(x=>x.endTime < Date.now()))
   let currentEvent = obj.shift()
@@ -12,6 +12,7 @@ module.exports = async(gaEvent = {})=>{
   let status = false
   for(let i in obj){
     if(obj[i].counterScanComplete) continue
+    if(!obj[i].historyScanComplete) continue
     log.info(`Setting counter scan for ${obj[i].date} ${obj[i].season} to complete...`)
     await mongo.set('gaEvents', { _id: obj[i]._id }, { counterScanComplete: true })
     if(obj[i].key === gaEvent?.key) status = true
